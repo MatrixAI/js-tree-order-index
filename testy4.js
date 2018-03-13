54ee4d9f60d1ab31d907b91c347a9527a038e6f2
Roger Qiu <roger.qiu@polyhack.io>
2018-03-13 20:43:44 +1100

    WIP

--- testy4.js
+++ testy4.js
@@ -8,20 +8,16 @@ const nodeTable = new NodeDataScript({
   keysIndexedObjectsTagSuffix: '-tag'
 });
 
-const boTree = new BOTree({
+const tree0 = new BOTree({
   blockSize: 4,
   nodeTable: nodeTable
 });
 
-
-// this appears to do it twice
-// why is the length and count wrong?
-
-const [node, tree] = boTree.insertRoot({
+const [node, tree1] = tree0.insertRoot({
   name: 'Hi I am Root!'
 });
 
-const [node2, tree2] = tree.insertRoot({
+const [node2, tree2] = tree1.insertRoot({
   name: 'I am the new root!'
 });
 
@@ -29,7 +25,7 @@ const [node3, tree3] = tree2.insertRoot({
   name: 'NAH I AM!'
 });
 
-console.log('tree3 inserting root');
+console.log('TREE4 PRODUCE');
 
 const [node4, tree4] = tree3.insertRoot({
   name: 'NOPE!'
@@ -37,30 +33,28 @@ const [node4, tree4] = tree3.insertRoot({
 
 // this should not have that many objects!
 
-console.log(tree3._treeTable);
-console.log(tree4._treeTable);
+console.log('TREE0', tree0._treeTable);
+console.log('TREE1', tree1._treeTable);
+// console.log(tree1._treeTable.get(0));
+console.log('TREE2', tree2._treeTable);
 
-console.log(tree4._leftId);
-console.log(tree4._rightId);
-console.log(tree4._rootId);
+console.log(tree2._treeTable.get(0).children._array);
+console.log(tree2._treeTable.get(1).children._array);
+console.log(tree2._treeTable.get(2).children._array);
 
-// prevId is wrong
+console.log('TREE3', tree3._treeTable);
 
-// node.split doesn't have this problem
-// only leaf.split has this problem
-// leaf split is the only one in which we need to fix this
-// one way is for split to actually return 3 things
-// leafLeft, leafRight, leafRightOrig
-// that is the original right (if it exist) has the id added as well
-// and it has it cloned
-// wait we only have the id
-// we don't have the existing block
-// damn we would need to acquire the block from the treeTableT
+console.log(tree3._treeTable.get(0));
+console.log(tree3._treeTable.get(1));
+console.log(tree3._treeTable.get(2));
 
+console.log('TREE4', tree4._treeTable);
 
-// why is tree4._treeTable.get(1) returning a Leaf with prevId set to 0
-// WHEN it should be set to 3
-// and it should be setttt!!?!?!?
+console.log('LENGTH of the table', tree4._treeTable.count());
+
+console.log('ROOTID', tree4._rootId);
+console.log('LEFTID', tree4._leftId);
+console.log('RIGHTID', tree4._rightId);
 
 console.log(tree4._treeTable.get(0));
 console.log(tree4._treeTable.get(1));
@@ -69,16 +63,8 @@ console.log(tree4._treeTable.get(3));
 console.log(tree4._treeTable.get(4));
 console.log(tree4._treeTable.get(5));
 console.log(tree4._treeTable.get(6));
-console.log(tree4._treeTable.get(7));
-console.log(tree4._treeTable.get(8));
-console.log(tree4._treeTable.get(9));
 
-// several things are broken here:
-// 1. the count for several ArrayFixedDense is SOMEHOW a floating point number
-//  HOW IS THAT EVEN POSSIBLE? (the count isn't even properly updated)
-// this must mean ArrayFixedDense must be broken
-// cause this is just impossible
-// there's nothing in this code that sets the count
-
-// it is only nodes with incorrect count
-// how did this occur?
+console.log(tree4._treeTable.get(0).children._array);
+console.log(tree4._treeTable.get(3).children._array);
+console.log(tree4._treeTable.get(4).children._array);
+console.log(tree4._treeTable.get(1).children._array);
