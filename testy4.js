d787a6d971175eb42a40e50b56fd55e7d77fc459
Roger Qiu <roger.qiu@polyhack.io>
2018-03-17 22:36:12 +1100

    WIP

--- testy4.js
+++ testy4.js
@@ -25,7 +25,7 @@ const [node3, tree3] = tree2.insertRoot({
   name: 'NAH I AM!'
 });
 
-console.log('TREE4 PRODUCE');
+// console.log('TREE4 PRODUCE');
 
 const [node4, tree4] = tree3.insertRoot({
   name: 'NOPE!'
@@ -33,36 +33,36 @@ const [node4, tree4] = tree3.insertRoot({
 
 // this should not have that many objects!
 
-console.log('TREE0', tree0._treeTable);
-console.log('TREE1', tree1._treeTable);
-// console.log(tree1._treeTable.get(0));
-console.log('TREE2', tree2._treeTable);
+// console.log('TREE0', tree0._treeTable);
+// console.log('TREE1', tree1._treeTable);
+// // console.log(tree1._treeTable.get(0));
+// console.log('TREE2', tree2._treeTable);
 
-console.log(tree2._treeTable.get(0).children._array);
-console.log(tree2._treeTable.get(1).children._array);
-console.log(tree2._treeTable.get(2).children._array);
+// console.log(tree2._treeTable.get(0).children._array);
+// console.log(tree2._treeTable.get(1).children._array);
+// console.log(tree2._treeTable.get(2).children._array);
 
-console.log('TREE3', tree3._treeTable);
+// console.log('TREE3', tree3._treeTable);
 
-console.log(tree3._treeTable.get(0));
-console.log(tree3._treeTable.get(1));
-console.log(tree3._treeTable.get(2));
+// console.log(tree3._treeTable.get(0));
+// console.log(tree3._treeTable.get(1));
+// console.log(tree3._treeTable.get(2));
 
-console.log('TREE4', tree4._treeTable);
+// console.log('TREE4', tree4._treeTable);
 
-console.log('LENGTH of the table', tree4._treeTable.count());
+// console.log('LENGTH of the table', tree4._treeTable.count());
 
-console.log('ROOTID', tree4._rootId);
-console.log('LEFTID', tree4._leftId);
-console.log('RIGHTID', tree4._rightId);
+// console.log('ROOTID', tree4._rootId);
+// console.log('LEFTID', tree4._leftId);
+// console.log('RIGHTID', tree4._rightId);
 
-console.log(tree4._treeTable.get(0));
-console.log(tree4._treeTable.get(1));
-console.log(tree4._treeTable.get(2));
-console.log(tree4._treeTable.get(3));
-console.log(tree4._treeTable.get(4));
-console.log(tree4._treeTable.get(5));
-console.log(tree4._treeTable.get(6));
+// console.log(tree4._treeTable.get(0));
+// console.log(tree4._treeTable.get(1));
+// console.log(tree4._treeTable.get(2));
+// console.log(tree4._treeTable.get(3));
+// console.log(tree4._treeTable.get(4));
+// console.log(tree4._treeTable.get(5));
+// console.log(tree4._treeTable.get(6));
 
 console.log(tree4._treeTable.get(0).children._array);
 console.log(tree4._treeTable.get(3).children._array);
